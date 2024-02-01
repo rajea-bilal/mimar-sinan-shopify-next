fd4f78e82b36e97e8991caff4538dfb8712032ed
rajea-bilal <rajea.bilal@gmail.com>
2024-02-01 10:55:27 +0000

    added logo to homepage

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import logoSource from '../../app/assets/logo4.png';
 
 export default function LogoIcon(props: React.ComponentProps<'svg'>) {
-  return <Image src={logoSource} width={60} height={60} alt="logo-image" />;
+  return <Image src={logoSource} width={80} height={60} alt="logo-image" className="logoBox" />;
 }
